5641fb0f4763b53a93d42076ec66910e7e4b3065
mcr <rayletmcr@gmail.com>
2024-03-14 14:12:42 +0100

    htmlportf

--- react/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import { qrcode } from "vite-plugin-qrcode";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), qrcode()],
-});
